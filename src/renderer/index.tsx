86272501704970b2a0c68d898245bb3c3ea166a9
Satchel <satchelspencer@gmail.com>
2019-07-06 14:27:42 -0600

    first commit webpack boilerplate

---
+++ src/renderer/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import ctyled from 'ctyled'
+
+const W = ctyled.div.styles({
+  bg: true,
+  padd: true,
+  border: 1
+})
+
+function App(){
+  return <W>www</W>
+}
+
+ReactDOM.render(<App/>, document.getElementById('app'))
